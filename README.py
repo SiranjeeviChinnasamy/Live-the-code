8d059c9633455c86c810c52add9dda122974e5df
SiranjeeviChinnasamy <45841627+SiranjeeviChinnasamy@users.noreply.github.com>
2018-12-13 12:31:57 +0530

    Update README.py

--- README.py
+++ README.py
@@ -1,5 +1,6 @@
 # Live-the-code
-#Create a program that asks the user for a number and then prints out a list of all the divisors of that number. (If you don’t know what a #divisor is, it is a number that divides evenly into another number.
+#Create a program that asks the user for a number and then prints out a list of all the divisors of that number. 
+#(If you don’t know what a #divisor is, it is a number that divides evenly into another number.)
 
 X = int(input('Enter a number: '))
 New_List = []
